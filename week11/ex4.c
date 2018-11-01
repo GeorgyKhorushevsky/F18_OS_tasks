b50e9bc028b36218b3088b37160a874fd0068036
GeorgyKhorushevsky <35738786+GeorgyKhorushevsky@users.noreply.github.com>
2018-11-01 22:42:26 +0300

    Add files via upload

--- week11/ex4.c
+++ week11/ex4.c
@@ -3,20 +3,26 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/mman.h>
+#include <sys/stat.h>
 
 int main() {
-    char *str_data = "Ono ne rabotaet\n";
     int str_size;
     int fd1;
     int fd2;
-    void *fm;
-    fd1 = open("ex1.txt", O_RDWR | O_TRUNC);
+    void *fm1;
+    void *fm2;
+    fd1 = open("ex1.txt", O_RDONLY);
     fd2 = open("ex1.memcpy.txt", O_RDWR | O_TRUNC);
-    *str_data = read(fd1, "", 1);
-    str_size = strlen(str_data);
-    lseek(fd2, str_size+1, SEEK_SET);
+
+    struct stat f1stat;
+
+    fstat(fd1, &f1stat);
+    str_size = f1stat.st_size;
+    lseek(fd2, str_size, SEEK_SET);
     write(fd2, "", 1);
-    fm = mmap(NULL, str_size, PROT_WRITE | PROT_READ, MAP_SHARED, fd2, 0);
-    memcpy(fm, str_data, str_size);
+
+    fm1 = mmap(NULL, str_size, PROT_READ, MAP_SHARED, fd1, 0);
+    fm2 = mmap(NULL, str_size, PROT_WRITE | PROT_READ, MAP_SHARED, fd2, 0);
+    memcpy(fm2, fm1, str_size);
     return 0;
 }
